0955eb5bce3a4eee97e32feaa03984504927385c
Dan <danpopa1998@gmail.com>
2017-12-12 13:53:02 +0200

    add second table

--- src/App.js
+++ src/App.js
@@ -17,13 +17,27 @@ import 'react-table/react-table.css'
   }
 `)
 class App extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      country: 'Germany'
+    }
+  }
+
+  setCountry= (country) => {
+    this.setState({
+      country
+    })
+  }
+
   render() {
     const data = this.props.data.allTableRows
     
     const columns = [
       {
         Header: 'Id',
-        accessor: 'id' // String-based value accessors!
+        accessor: 'id', // String-based value accessors!
+        
       },
       {
         Header: 'name',
@@ -39,7 +53,8 @@ class App extends Component {
       },
       {
         Header: 'birthCountry',
-        accessor: 'birthCountry' // String-based value accessors!
+        accessor: 'birthCountry', // String-based value accessors!
+        Cell: props => <span className='number' onClick={() => this.setCountry(props.value)}>{props.value}</span>
       },
       {
         Header: 'createdAt',
@@ -47,9 +62,39 @@ class App extends Component {
       },
     ]
 
+    const columnsCountryFilter = [
+      {
+        Header: 'Id',
+        accessor: 'id', // String-based value accessors!
+        
+      },
+      {
+        Header: 'name',
+        accessor: 'name' // String-based value accessors!
+      },
+      {
+        Header: 'surname',
+        accessor: 'surname' // String-based value accessors!
+      },
+      {
+        Header: ' age',
+        accessor: 'age' // String-based value accessors!
+      },
+      {
+        Header: 'birthCountry',
+        accessor: 'birthCountry', // String-based value accessors!
+      },
+      {
+        Header: 'createdAt',
+        accessor: 'createdAt' // String-based value accessors!
+      },
+    ]
+    const dataCountryFilter = this.props.data.allTableRows &&  
+      this.props.data.allTableRows.filter(el => el.birthCountry === this.state.country)
     return (
       <div>
         <ReactTable data={data} columns={columns}/>
+        <ReactTable data={dataCountryFilter} columns={columnsCountryFilter}/>
       </div>
     )
   }
